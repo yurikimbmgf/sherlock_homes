b1dd3412013ea78e719e6a36fa033c76c82a32e4
Isabella Velásquez <ivelasq@gmail.com>
2020-09-15 21:42:18 -0700

    half working

--- server.R
+++ server.R
@@ -52,33 +52,32 @@ gs4_auth(
 # Loading functions
 source("00_function.R") 
 
-
 # Server ------------------------------------------------------------------
 server <- function(input, output) {
   
   # Run mls_scrape() and then add the ratings.
-  new_full_entry_df <- eventReactive(input$submit_url_actionbutton, {
-    mls_scrape(input$mls_number_textinput) %>% 
+  observeEvent(input$submit_url_actionbutton, {
+    
+    new_full_entry_df <- 
+      mls_scrape(input$mls_number_textinput) %>% 
       add_row(mls = input$mls_number_textinput, attribute = "Location Rating", value = as.character(input$location_rating)) %>%
       add_row(mls = input$mls_number_textinput, attribute = "Price Rating", value = as.character(input$price_rating)) %>%
       add_row(mls = input$mls_number_textinput, attribute = "Place Rating", value = as.character(input$place_rating))
+    
+    new_essential_entry_df <- 
+      new_full_entry_df %>% 
+      essentials_function()
+    
+    output$new_full_dt <- DT::renderDataTable({
+      new_full_entry_df
   })
-  
-  # Runs the essential fucntions
-  new_essential_entry_df <- eventReactive(input$submit_url_actionbutton, {
-    essentials_function(new_full_entry_df())
-  })
-  
-  # Wrapper to make the new_full_entry_df a datatable
-  output$new_full_dt <- renderDataTable({
-    new_full_entry_df()
   })
   
   # Saving the data to google drive
-  observeEvent(
-    input$save_to_drive,{
-      googlesheets4::sheet_append(new_full_entry_df(), ss = "https://docs.google.com/spreadsheets/d/1-3zc3pxaxipcvpydSBzPiZ_iZDismmZRMKaCuAPdYIs/edit#gid=1751745956", sheet = "entry_full_seed")
-      googlesheets4::sheet_append(new_essential_entry_df(), ss = "https://docs.google.com/spreadsheets/d/1wxAF8AkZLcNl1uD05ByOmZ9ffMxTGeVHgljUtOkzgsM/edit#gid=60631602", sheet = "entry_full_essential")
+  observeEvent(input$save_to_drive, {
+    
+      googlesheets4::sheet_append(new_full_entry_df, ss = "https://docs.google.com/spreadsheets/d/1-3zc3pxaxipcvpydSBzPiZ_iZDismmZRMKaCuAPdYIs/edit#gid=1751745956", sheet = "entry_full_seed")
+      googlesheets4::sheet_append(new_essential_entry_df, ss = "https://docs.google.com/spreadsheets/d/1wxAF8AkZLcNl1uD05ByOmZ9ffMxTGeVHgljUtOkzgsM/edit#gid=60631602", sheet = "entry_full_essential")
     })
   
   # I don't really know how this works.
